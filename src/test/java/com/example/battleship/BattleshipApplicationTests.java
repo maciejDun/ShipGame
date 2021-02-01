c9138792d08750f99cdf87521763a4da249a62bb
Maciek <bambuzlator3@gmail.com>
2021-02-01 17:33:54 +0100

    initial commit

---
+++ src/test/java/com/example/battleship/BattleshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.battleship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
